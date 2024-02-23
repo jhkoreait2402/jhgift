33f6399210943dfe012f4d86fe6a9d16c73c33df
admin <admin@DESKTOP-8VOVB0M>
2024-02-23 14:51:31 +0900

    HI

---
+++ s2/target/classes/dto/MemberDTO.java
@@ -0,0 +1,13 @@
+package dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDTO {
+
+	int m_idx;
+	String m_id, m_pwd, m_name, m_address, m_email, m_phone; // '000-0000-0000'
+}
